57e690144a6d343b1bab380edde5302fa7f5cc91
Andre Meira <andre.meira@amaggi.com.br>
2023-11-30 14:21:24 -0400

    Adicionando estrutura de log.

---
+++ src/Domain/Domain.Core/Observability/Logger.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Exceptions;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Domain.Core.Observability;
+
+public static class LoggerConfiguration
+{
+    public static ConfigureHostBuilder AddLogginSerilog(
+        this ConfigureHostBuilder host, string nameService)
+    {
+        host.UseSerilog((context, configuration) =>
+        {
+            configuration.WriteTo
+                .Console(outputTemplate:"[{Timestamp:HH:mm:ss} {Level:u3} {SourceContext} {Properties:j} Mensagem:{Message:lj}{NewLine}{Exception}");
+            
+            configuration.MinimumLevel.Override("Microsoft", LogEventLevel.Error);
+            configuration.MinimumLevel.Override("Microsoft.AspNetCore.Hosting.Diagnostics", LogEventLevel.Warning);
+            
+            configuration.Enrich.FromLogContext();            
+            configuration.Enrich.WithExceptionDetails();
+            configuration.Enrich.WithClientIp();
+            configuration.Enrich.WithClientAgent();
+            configuration.Enrich.WithProperty("name_service", nameService);
+
+        });
+
+        return host;
+    }
+
+    public static IServiceCollection ConfigureLogging(this IServiceCollection services)
+    {
+        services.AddLogging(e =>
+        {
+            e.AddSerilog(logger: Log.Logger);
+            e.AddEventSourceLogger();            
+        });
+
+        return services;
+    }
+}
+
+partial class TraceIdEnricher : ILogEventEnricher
+{
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+    {
+        var currentActivity = Activity.Current;
+        if (currentActivity == null) return;
+
+        var traceIdProperty = propertyFactory.CreateProperty("trace_id", currentActivity.TraceId);
+        var spanIdProperty = propertyFactory.CreateProperty("span_id", currentActivity.SpanId);                
+
+        logEvent.AddPropertyIfAbsent(traceIdProperty);
+        logEvent.AddPropertyIfAbsent(spanIdProperty);                
+    }
+}
+
--- src/Services/Payment/Payment.API/Program.cs
+++ src/Services/Payment/Payment.API/Program.cs
@@ -1,28 +1,35 @@
 using Domain.Core.Observability;
 using MassTransit;
+using Microsoft.Extensions.Logging;
 using Payment.Core;
 using Payment.Core.Orchestration;
 using Payment.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
+string nameService = "Payment.API";
+
+builder.Host.AddLogginSerilog(nameService);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddTracing("Payment.API",builder.Configuration);
+builder.Services.ConfigureLogging();
+builder.Services.AddTracing(nameService,builder.Configuration);
 
 builder.Services.ConfigureInfrastructure(builder.Configuration);
 builder.Services.AddWorkerService(builder.Configuration);
 
+
 builder.Services.AddMassTransit(e =>
 {
     e.SetKebabCaseEndpointNameFormatter();
-
+    
     e.AddConsumer<OrchestrationWoker>(typeof(OrchestrationWokerDefinition));
 
     e.UsingRabbitMq((context, cfg) =>
-    {
+    {        
+
         cfg.Host("localhost", "/", h =>
         {
             h.Username("guest");
@@ -35,14 +42,11 @@ builder.Services.AddMassTransit(e =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
 
-app.UseHttpsRedirection();
+app.UseSwaggerUI();
+
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
